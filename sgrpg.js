e80bd7e529a9d85ea9231ebd017ac4e35fa3e4a0
Hertzila <Hertzila@users.noreply.github.com>
2021-09-06 04:52:39 +0300

    Drag Ruler integration

--- sgrpg.js
+++ sgrpg.js
@@ -87,3 +87,34 @@ function getTensionDie() {
     const tensionDie = game.scenes.active.getFlag("sgrpg", "sceneTensionDie") || game.settings.get("sgrpg", "campaignTension");
     return tensionDie;
 }
+
+
+/* -------------------------------------------- */
+/*  External Module Support                     */
+/* -------------------------------------------- */
+
+// Drag Ruler integration
+Hooks.once("dragRuler.ready", (SpeedProvider) => {
+    class SGSpeedProvider extends SpeedProvider {
+        get colors() {
+            return [
+                { id: "walk", default: 0x0000FF, name: "Walking Speed" },
+                { id: "dash", default: 0x00DE00, name: "Dashing Speed" }
+            ];
+        }
+
+        getRanges(token) {
+            const walkspeed = token.actor?.data.data.speed || 0;
+            const dashspeed = walkspeed * 2;
+
+            const ranges = [
+                { range: walkspeed, color: "walk" },
+                { range: dashspeed, color: "dash" }
+            ];
+
+            return ranges;
+        }
+    }
+
+    dragRuler.registerSystem("sgrpg", SGSpeedProvider);
+});
